62d49f6d944782090e6d9444cb98295638438a56
mohammadishker <mhdeshker@gmail.com>
2024-03-08 17:14:29 +0200

    Initial Commit

---
+++ Swift-UI App/Swift_UI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_AppApp.swift
+//  Swift-UI App
+//
+//  Created by Mohammad Ishker on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
