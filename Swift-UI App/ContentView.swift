004f57f924d2c9fa9cc92bee03fcb9bd7b96909e
Mohammad <mhdeshker@gmail.com>
2024-11-05 04:15:56 +0300

    First app / Bulls_eye_app: initial UI elements

--- Swift-UI App/ContentView.swift
+++ Swift-UI App/ContentView.swift
@@ -10,10 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .multilineTextAlignment(.center)
+                .lineSpacing(10.0)
+                .font(.footnote)
+                .kerning(2.0)
+                .bold()
+            Text("89")
+            HStack{
+                Text("1")
+                Slider(value: .constant(50), in: 1 ... 100)
+                Text("100")
+            }
+            Button("Hit me") {
+            }
         }
         .padding()
     }
