d992a16bc73b4807aa99f0dbcdea709ce265f9f9
Mohammad <mhdeshker@gmail.com>
2024-11-08 09:11:55 +0300

    Polishing the App/ ui padding, colors and gradients

--- Swift-UI App/Views/ContentView.swift
+++ Swift-UI App/Views/ContentView.swift
@@ -13,42 +13,57 @@ struct ContentView: View {
     @State private var sliderValue = 50.0
     @State private var game = Game()
     var body: some View {
-        VStack {
-            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                .multilineTextAlignment(.center)
-                .lineSpacing(10.0)
-                .font(.footnote)
-                .kerning(2.0)
+        ZStack {
+            Color("BackgroundColor").ignoresSafeArea()
+            VStack {
+                Text("🎯🎯🎯\nPut the bullseye as close as you can to".uppercased())
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(10.0)
+                    .font(.footnote)
+                    .kerning(2.0)
+                    .bold()
+                    .padding(.horizontal, 30)
+                Text(String(game.target))
+                    .font(.largeTitle)
+                    .kerning(-1.0)
+                    .fontWeight(.black)
+                HStack{
+                    Text("1").bold()
+                    Slider(value: $sliderValue, in: 1 ... 100)
+                    Text("100").bold()
+                }.padding(.all)
+                Button("Hit me".uppercased()) {
+                    print("Alret is opened")
+                    isAlertVisible = true
+                }
                 .bold()
-            Text(String(game.target))
-                .font(.largeTitle)
-                .kerning(-1.0)
-                .fontWeight(.black)
-            HStack{
-                Text("1").bold()
-                Slider(value: $sliderValue, in: 1 ... 100)
-                Text("100").bold()
-            }
-            Button("Hit me") {
-                print("Alret is opened")
-                isAlertVisible = true
-            }
-            .alert(
-                "Hello there!",
-                isPresented: $isAlertVisible,
-                actions: {
-                    Button("Awesome") {
-                        print("Alret is closed")
+                .font(.title3)
+                .padding(21.0)
+                .background(
+                    ZStack{
+                        Color("ButtonColor")
+                        LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
                     }
-                },
-                message: { Text ("""
-your target is \(game.target)
-your guess is \(Int(sliderValue.rounded()))
-you collected \(game.points(sliderValue: Int(sliderValue.rounded()))) points this round
-""") }
-            )
+                )
+                .foregroundColor(Color.white)
+                .cornerRadius(21.0)
+                .alert(
+                    "Hello there!",
+                    isPresented: $isAlertVisible,
+                    actions: {
+                        Button("Awesome") {
+                            print("Alret is closed")
+                        }
+                    },
+                    message: { Text ("""
+    your target is \(game.target)
+    your guess is \(Int(sliderValue.rounded()))
+    you collected \(game.points(sliderValue: Int(sliderValue.rounded()))) points this round
+    """) }
+                )
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
